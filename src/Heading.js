0315d15444bc410cdb541098bc08ac0f317212e8
BPitts8019 <bradleyj.pitts@gmail.com>
2021-07-06 20:51:27 -0700

    Destructure props in Heading component

--- src/Heading.js
+++ src/Heading.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-function Heading(props) {
-   const message = "Hello World!!";
-   return <h1>{props.message}</h1>;
+function Heading({ message }) {
+   return <h1>{message}</h1>;
 }
 
 export default Heading;
